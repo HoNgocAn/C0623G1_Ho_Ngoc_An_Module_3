bb734fa27f8a361d768e4b71967f11429391acac
HoNgocAn <anhongoc1306@gmail.com>
2023-09-11 15:13:38 +0700

    nop bai

---
+++ ss1_csdl/qlsv.sql
@@ -0,0 +1,20 @@
+create database demo;
+use demo;
+create table student(
+ studentId int,
+ studentName varchar(200),
+ studentAge int,
+ studentCountry varchar(50)
+);
+create table class(
+ classId int,
+ className varchar(200)
+);
+create table teacher(
+ teacherId int,
+ teacherName varchar(200),
+ teacherAge int,
+ teacherCountry varchar(50)
+);
+
+
