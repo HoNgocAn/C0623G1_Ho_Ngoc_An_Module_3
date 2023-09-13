18b9b5357034ee969ae2124e1de203114adc0704
HoNgocAn <anhongoc1306@gmail.com>
2023-09-13 21:10:55 +0700

    nop bai

---
+++ ss3_thaotac_CSDL/qlsvSS3.sql
@@ -0,0 +1,17 @@
+USE quanlysinhvien;
+SELECT * 
+FROM student
+WHERE StudentName LIKE "H%";
+
+SELECT * 
+FROM subjects
+WHERE Credit >= 3 AND Credit <=5 ;
+
+UPDATE student 
+SET ClassId = 2
+WHERE StudentName = 'Hung';
+
+SELECT S.StudentName, Su.SubName, M.Mark
+FROM student s jOIN mark m ON s.StudentId = m.MarkId 
+JOIN subjects Su ON m.MarkId = su.SubId
+ORDER BY mark DESC;
