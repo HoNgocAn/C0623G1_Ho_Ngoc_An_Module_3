4afb655bc1fb4c9300da982436c9be097601f6a2
HoNgocAn <anhongoc1306@gmail.com>
2023-09-16 10:01:58 +0700

    nop bai

--- castudyDB/11_15casestudyDB.sql
+++ castudyDB/11_15casestudyDB.sql
@@ -1,7 +1,7 @@
 USE casestudy;
 
 -- Câu 11
-SELECT*
+SELECT *
 FROM dich_vu_di_kem dk
 JOIN hop_dong_chi_tiet hct
 ON dk.ma_dich_vu_di_kem = hct.ma_dich_vu_di_kem
@@ -14,7 +14,7 @@ ON k.ma_loai_khach = l.ma_loai_khach
 WHERE l.ten_loai_khach = "Diamond" AND (k.dia_chi LIKE "%Vinh" OR k.dia_chi LIKE "%Quảng Ngãi");
 
 -- Câu 12
-SELECT h.ma_hop_dong, n.ho_ten, k.ho_ten,k.so_dien_thoai,d.ten_dich_vu, h.tien_dat_coc , sum(hct.so_luong) AS "Số lượng dịch vụ đi k"
+SELECT h.ma_hop_dong, n.ho_ten, k.ho_ten,k.so_dien_thoai,d.ten_dich_vu, h.tien_dat_coc , sum(hct.so_luong) AS "Số lượng dịch vụ đi kèm"
 FROM hop_dong h
 JOIN khach_hang k ON h.ma_khach_hang = k.ma_khach_hang
 JOIN nhan_vien n ON h.ma_nhan_vien = n.ma_nhan_vien
@@ -35,10 +35,10 @@ ON h.ma_khach_hang = k.ma_khach_hang
 GROUP BY dk.ma_dich_vu_di_kem
 HAVING count(dk.ma_dich_vu_di_kem)
 ORDER BY count(dk.ma_dich_vu_di_kem) DESC
-LIMIT 1;
+LIMIT 3;
 
 -- Câu 14
-SELECT h.ma_hop_dong, l.ten_loai_dich_vu, dk.ten_dich_vu_di_kem, count(dk.ma_dich_vu_di_kem)
+SELECT h.ma_hop_dong, l.ten_loai_dich_vu, dk.ten_dich_vu_di_kem, count(dk.ma_dich_vu_di_kem) AS"Số lần sử dụng"
 FROM dich_vu_di_kem dk
 JOIN hop_dong_chi_tiet hct
 ON dk.ma_dich_vu_di_kem = hct.ma_dich_vu_di_kem
@@ -53,15 +53,13 @@ HAVING  count(dk.ma_dich_vu_di_kem) =1;
 
 -- Câu 15
 
-SELECT n.ma_nhan_vien, n.ho_ten, t.ten_trinh_do, b.ten_bo_phan, n.so_dien_thoai, n.dia_chi, count(n.ho_ten)
+SELECT n.ma_nhan_vien, n.ho_ten, t.ten_trinh_do, b.ten_bo_phan, n.so_dien_thoai, n.dia_chi, count(n.ho_ten) AS "Số lượng hợp đồng"
 FROM hop_dong h
 JOIN nhan_vien n ON n.ma_nhan_vien = h.ma_nhan_vien
 JOIN trinh_do t ON n.ma_trinh_do = t.ma_trinh_do
 JOIN bo_phan b ON b.ma_bo_phan = n.ma_bo_phan
 WHERE h.ngay_lam_hop_dong>= "2020-1-1" AND h.ngay_lam_hop_dong<="2021-12-31"
 GROUP BY n.ma_nhan_vien, n.ho_ten, t.ten_trinh_do, b.ten_bo_phan, n.so_dien_thoai, n.dia_chi
-HAVING count(n.ho_ten)<=3
-
-
+HAVING count(n.ho_ten)<=3;
 
 
--- castudyDB/2_5casestudyDB.sql
+++ castudyDB/2_5casestudyDB.sql
@@ -1,12 +1,14 @@
 USE casestudy;
 
+-- Câu 2
 SELECT * 
 FROM nhan_vien
-WHERE LENGTH(ho_ten) <=15 
+WHERE CHAR_LENGTH(ho_ten) <=15 
 AND (SUBString_INDEX(ho_ten,' ',-1) LIKE 'T%' 
 OR SUBString_INDEX(ho_ten,' ',-1) LIKE 'H%' 
 OR SUBString_INDEX(ho_ten,' ',-1) LIKE 'K%');
 
+-- Câu 3
 SELECT * 
     FROM khach_hang
     WHERE
@@ -15,7 +17,8 @@ SELECT *
     AND (dia_chi LIKE '%Đà Nẵng' 
     OR dia_chi LIKE '%Quảng Trị');
     
-SELECT k.*, count(k.ma_khach_hang) 
+-- Câu 4
+SELECT k.*, count(k.ma_khach_hang) AS "Số lần đặt phòng"
 FROM khach_hang k
 JOIN loai_khach l
 ON	k.ma_loai_khach=l.ma_loai_khach
@@ -24,3 +27,14 @@ ON k.ma_khach_hang=h.ma_khach_hang
 WHERE l.ten_loai_khach="diamond"
 GROUP BY k.ma_khach_hang
 ORDER BY count(k.ma_khach_hang); 
+
+-- Câu 5
+SELECT k.ma_khach_hang, k.ho_ten, l.ten_loai_khach, h.ma_hop_dong, d.ten_dich_vu, h.ngay_lam_hop_dong, h.ngay_ket_thuc
+FROM hop_dong h
+RIGHT JOIN khach_hang k 
+ON h.ma_khach_hang = k.ma_khach_hang
+JOIN dich_vu d 
+ON h.ma_dich_vu = d.ma_dich_vu
+JOIN loai_khach l
+ON k.ma_loai_khach = l.ma_loai_khach;
+
