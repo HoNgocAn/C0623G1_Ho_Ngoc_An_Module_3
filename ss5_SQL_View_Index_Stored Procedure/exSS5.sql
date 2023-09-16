acc87a5c2f6d1d60ea1a257bdd086a75a98871ed
HoNgocAn <anhongoc1306@gmail.com>
2023-09-16 17:10:58 +0700

    nop bai

--- ss5_SQL_View_Index_Stored Procedure/exSS5.sql
+++ ss5_SQL_View_Index_Stored Procedure/exSS5.sql
@@ -13,15 +13,14 @@ CREATE TABLE products (
 
 insert into products(product_code,product_name,product_price,product_amount,product_description,product_status)
 values
-('P001','Áo thun nam','150000','50','Áo thun nam kiểu dáng đơn giản và thoải mái',1),
-('P002','Quần jeans nữ','300000','30','Quần jeans nữ có kiểu dáng phong cách và chất lượng tốt',1),
-('P003','Đầm dạo phố','250000','0','Đầm dạo phố thời trang và thoải mái',0);
+('A1','Áo phong','100000','10','Thoải mái',1),
+('A2','Váy nữ','200000','5','Chất lượng tốt',0);
 
 -- Thao tác với index
-EXPLAIN SELECT * FROM products WHERE product_name = 'Áo thun nam'; 
+EXPLAIN SELECT * FROM products WHERE product_name = 'Áo phong'; 
 ALTER TABLE products ADD INDEX index_product_code(product_code);
 ALTER TABLE products ADD INDEX index_name_price(product_name, product_price);
-EXPLAIN SELECT * FROM products WHERE product_name = 'Áo thun nam'; 
+EXPLAIN SELECT * FROM products WHERE product_name = 'Áo phong'; 
 
 DROP INDEX index_name_price
 ON products;
@@ -70,7 +69,7 @@ VALUES(product_code,product_name,product_price,product_amount,product_descriptio
 END //
 DELIMITER ;
 
-CALL addNewProduct('P004','Vay dạo phố','250000','0','Vay dạo phố thời trang và thoải mái',0);
+CALL addNewProduct('A3','Vest','300000','0','Lịch Lãm',1);
 
 -- Update san pham
 DELIMITER //
@@ -95,7 +94,7 @@ BEGIN
 END //
 DELIMITER ;
 
-CALL editProduct(4,'P004','Vay xinh dạo phố','250000','0','Vay dạo phố thời trang và thoải mái',0);
+CALL editProduct('A3','Vest','300000','0','Lịch Lãm',1);
 
 -- Xoa san pham theo id
 DELIMITER //
