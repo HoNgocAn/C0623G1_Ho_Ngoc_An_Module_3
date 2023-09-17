0202c85e798f205f5b3913aa5e2253cea24c6370
HoNgocAn <anhongoc1306@gmail.com>
2023-09-17 22:42:22 +0700

    nop bai

--- castudyDB/11_15casestudyDB.sql
+++ castudyDB/11_15casestudyDB.sql
@@ -1,7 +1,7 @@
 USE casestudy;
 
 -- Câu 11
-SELECT *
+SELECT dk.*,k.ho_ten,k.dia_chi,l.ten_loai_khach
 FROM dich_vu_di_kem dk
 JOIN hop_dong_chi_tiet hct
 ON dk.ma_dich_vu_di_kem = hct.ma_dich_vu_di_kem
@@ -14,14 +14,19 @@ ON k.ma_loai_khach = l.ma_loai_khach
 WHERE l.ten_loai_khach = "Diamond" AND (k.dia_chi LIKE "%Vinh" OR k.dia_chi LIKE "%Quảng Ngãi");
 
 -- Câu 12
-SELECT h.ma_hop_dong, n.ho_ten, k.ho_ten,k.so_dien_thoai,d.ten_dich_vu, h.tien_dat_coc , sum(hct.so_luong) AS "Số lượng dịch vụ đi kèm"
+SELECT h.ma_hop_dong, n.ho_ten AS "Tên nhân viên", k.ho_ten AS "Tên khách hàng", k.so_dien_thoai AS "SĐT khách hàng",
+d.ten_dich_vu, h.tien_dat_coc , sum(hct.so_luong) AS "Số lượng dịch vụ đi kèm"
 FROM hop_dong h
 JOIN khach_hang k ON h.ma_khach_hang = k.ma_khach_hang
 JOIN nhan_vien n ON h.ma_nhan_vien = n.ma_nhan_vien
 JOIN dich_vu d ON h.ma_dich_vu = d.ma_dich_vu
-JOIN hop_dong_chi_tiet hct ON h.ma_hop_dong = hct.ma_hop_dong
+LEFT JOIN hop_dong_chi_tiet hct ON h.ma_hop_dong = hct.ma_hop_dong
 WHERE QUARTER(h.ngay_lam_hop_dong) = 4 AND YEAR(h.ngay_lam_hop_dong) = 2020
-GROUP BY h.ma_hop_dong, n.ho_ten, k.ho_ten,k.so_dien_thoai,d.ten_dich_vu, h.tien_dat_coc;
+AND d.ma_dich_vu NOT IN 
+(SELECT ma_dich_vu
+FROM hop_dong
+WHERE ngay_lam_hop_dong BETWEEN "2021-1-1" AND "2021-6-30")
+GROUP BY h.ma_hop_dong;
 
 -- Câu 13
 SELECT dk.*, count(dk.ma_dich_vu_di_kem) AS "Số lần sử dụng dịch vụ đi kèm"
@@ -38,7 +43,7 @@ ORDER BY count(dk.ma_dich_vu_di_kem) DESC
 LIMIT 3;
 
 -- Câu 14
-SELECT h.ma_hop_dong, l.ten_loai_dich_vu, dk.ten_dich_vu_di_kem, count(dk.ma_dich_vu_di_kem) AS"Số lần sử dụng"
+SELECT dk.ten_dich_vu_di_kem, count(dk.ma_dich_vu_di_kem) AS "Số lần sử dụng"
 FROM dich_vu_di_kem dk
 JOIN hop_dong_chi_tiet hct
 ON dk.ma_dich_vu_di_kem = hct.ma_dich_vu_di_kem
@@ -48,8 +53,8 @@ JOIN dich_vu d
 ON h.ma_dich_vu = d.ma_dich_vu
 JOIN loai_dich_vu l 
 ON d.ma_loai_dich_vu = l.ma_loai_dich_vu
-GROUP BY dk.ma_dich_vu_di_kem,h.ma_hop_dong, l.ten_loai_dich_vu
-HAVING  count(dk.ma_dich_vu_di_kem) =1;
+GROUP BY dk.ma_dich_vu_di_kem
+HAVING  count(dk.ma_dich_vu_di_kem) = 1;
 
 -- Câu 15
 
@@ -59,7 +64,7 @@ JOIN nhan_vien n ON n.ma_nhan_vien = h.ma_nhan_vien
 JOIN trinh_do t ON n.ma_trinh_do = t.ma_trinh_do
 JOIN bo_phan b ON b.ma_bo_phan = n.ma_bo_phan
 WHERE h.ngay_lam_hop_dong>= "2020-1-1" AND h.ngay_lam_hop_dong<="2021-12-31"
-GROUP BY n.ma_nhan_vien, n.ho_ten, t.ten_trinh_do, b.ten_bo_phan, n.so_dien_thoai, n.dia_chi
+GROUP BY n.ma_nhan_vien
 HAVING count(n.ma_nhan_vien)<=3;
 
 
--- castudyDB/6_10casestudyDB.sql
+++ castudyDB/6_10casestudyDB.sql
@@ -1,6 +1,7 @@
 USE casestudy;
 
 -- Câu 6
+
 SELECT h.ma_dich_vu, d.ten_dich_vu, d.dien_tich, d.chi_phi_thue, l.ten_loai_dich_vu, h.ngay_lam_hop_dong
 FROM hop_dong h 
 JOIN dich_vu d 
@@ -17,28 +18,47 @@ JOIN dich_vu d
 ON h.ma_dich_vu = d.ma_dich_vu
 JOIN loai_dich_vu l
 ON d.ma_loai_dich_vu = l.ma_loai_dich_vu
-WHERE YEAR (h.ngay_lam_hop_dong) = 2020 ;
+WHERE d.ma_dich_vu 
+IN (SELECT ma_dich_vu  
+FROM hop_dong 
+WHERE YEAR(ngay_lam_hop_dong) = "2020")
+AND d.ma_dich_vu  NOT IN (SELECT ma_dich_vu   
+FROM hop_dong 
+WHERE YEAR(ngay_lam_hop_dong) = "2021");
 
 -- --Câu 8
+
+-- Cách 1
 SELECT DISTINCT ho_ten 
 FROM khach_hang;
 
+-- Cách 2
+SELECT k.ho_ten 
+FROM khach_hang k
+UNION
+SELECT k.ho_ten 
+FROM khach_hang k;
+
+-- Cách 3
+SELECT k.ho_ten
+FROM khach_hang k
+GROUP BY k.ho_ten;
+
 -- Câu 9
 
-SELECT h.ngay_lam_hop_dong, h.tien_dat_coc,k.ho_ten
-FROM khach_hang k
-JOIN hop_dong h
-ON k.ma_khach_hang = h.ma_khach_hang
+SELECT MONTH(h.ngay_lam_hop_dong) AS "Tháng có khách đặt phòng trong năm 2021",count(MONTH(h.ngay_lam_hop_dong)) AS "Số khách hàng đặt phòng"
+FROM hop_dong h
 WHERE YEAR(h.ngay_lam_hop_dong) = 2021
-ORDER BY h.ngay_lam_hop_dong;
+GROUP BY MONTH(h.ngay_lam_hop_dong)
+ORDER BY MONTH(h.ngay_lam_hop_dong);
 
 -- Câu 10
 
 SELECT h.ma_hop_dong, h.ngay_lam_hop_dong, h.ngay_ket_thuc, h.tien_dat_coc, sum(hct.so_luong) as "So_luong_dich_vu_di_kem"
 FROM hop_dong h
-JOIN hop_dong_chi_tiet hct 
+LEFT JOIN hop_dong_chi_tiet hct 
 ON h.ma_hop_dong = hct.ma_hop_dong
-JOIN dich_vu_di_kem dv 
+LEFT JOIN dich_vu_di_kem dv 
 ON hct.ma_dich_vu_di_kem = dv.ma_dich_vu_di_kem
 GROUP BY h.ma_hop_dong;
 
